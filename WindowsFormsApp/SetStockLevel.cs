b5da0f9e7c0b6b000e29f64f7699b3e514a1562a
blackwind999 <chungricky858@gmail.com>
2024-06-10 01:01:21 +0800

    upup

--- WindowsFormsApp/SetStockLevel.cs
+++ WindowsFormsApp/SetStockLevel.cs
@@ -56,8 +56,31 @@ namespace WindowsFormsApp
 
         private void comboBoxSpareID_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string sql = $"select StaffID FROM User WHERE UserID = {Main.userID};";
+            int StaffID = 0;
+            using (var reader = Main.db.readBySql(sql))
+            {
+                if (reader.Read())
+                {
+                    StaffID = reader.GetInt32(0);
+                }
+            }
+            sql = $"SELECT w.WarehouseID FROM Staff s JOIN Department d ON s.DeptID = d.DeptID JOIN Warehouse w ON d.WarehouseID = w.WarehouseID WHERE s.StaffID = {StaffID}";
+            int WarehouseID = 0;
+            using (var reader = Main.db.readBySql(sql))
+            {
+                if (reader.Read())
+                {
+                    WarehouseID = reader.GetInt32(0);
+                }
+            }
+            int ROL = 0;
+            int CSL = 0;
+            int DL = 0;
+
+
             string SpareIDD = comboBoxSpareID.SelectedItem.ToString();
-            string sql = $"SELECT SpareName FROM Spare WHERE SpareID = '{SpareIDD}';";
+            sql = $"SELECT SpareName FROM Spare WHERE SpareID = '{SpareIDD}';";
             string spareName = String.Empty;
             using (var reader = Main.db.readBySql(sql))
             {
@@ -67,6 +90,43 @@ namespace WindowsFormsApp
                 }
             }
             txtSpareName.Text = spareName;
+            sql = $"SELECT ROL FROM WarehouseStockLevel WHERE SpareID = '{SpareIDD}' AND WarehouseID = {WarehouseID}";
+            using (var reader = Main.db.readBySql(sql))
+            {
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0))
+                    {
+                        ROL = reader.GetInt32(0);
+                    }
+                }
+            }
+            txtReOrderLevel.Text = ROL.ToString();
+            sql = $"SELECT DL FROM WarehouseStockLevel WHERE SpareID = '{SpareIDD}' AND WarehouseID = {WarehouseID}";
+            using (var reader = Main.db.readBySql(sql))
+            {
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0))
+                    {
+                        DL = reader.GetInt32(0);
+                    }
+                }
+            }
+            txtDangerLevel.Text = DL.ToString();
+            sql = $"SELECT CSL FROM WarehouseStockLevel WHERE SpareID = '{SpareIDD}' AND WarehouseID = {WarehouseID}";
+            using (var reader = Main.db.readBySql(sql))
+            {
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0))
+                    {
+                        CSL = reader.GetInt32(0);
+                    }
+                }
+            }
+            txtCommonStockLevel.Text = CSL.ToString();
+
         }
 
         private void btnSave_Click(object sender, EventArgs e)
@@ -95,11 +155,23 @@ namespace WindowsFormsApp
                 {
                     int ReOrderLevel;
                     int CommonStockLevel;
+                    int DangerLevel;
                     if (int.TryParse(txtReOrderLevel.Text, out ReOrderLevel) &&
-                      int.TryParse(txtCommonStockLevel.Text, out CommonStockLevel))
+                      int.TryParse(txtCommonStockLevel.Text, out CommonStockLevel) &&
+                      int.TryParse(txtDangerLevel.Text, out DangerLevel))
                     {
                         string id = comboBoxSpareID.SelectedItem.ToString();
-                        sql = $"UPDATE WarehouseStockLevel SET ROL = {ReOrderLevel},  CSL = {CommonStockLevel} WHERE SpareID = '{id}' AND WarehouseID = {WarehouseID}";
+                        if (checkBoxAutoReStock.Checked)
+                        {
+                            sql = $"UPDATE ActualStock SET AutoRestork = 1 WHERE SpareID = '{id}' AND WarehouseID = {WarehouseID}";
+
+                        }
+                        else
+                        {
+                            sql = $"UPDATE ActualStock SET AutoRestork = 0 WHERE SpareID = '{id}' AND WarehouseID = {WarehouseID}";
+                        }
+                        Main.db.updateBySql(sql);
+                        sql = $"UPDATE WarehouseStockLevel SET ROL = {ReOrderLevel},  CSL = {CommonStockLevel},  DL = {DangerLevel} WHERE SpareID = '{id}' AND WarehouseID = {WarehouseID}";
                         Main.db.updateBySql(sql);
                         Main.ShowYesNoDialog("Setting Successful!");
                     }
@@ -132,6 +204,29 @@ namespace WindowsFormsApp
             {
                 txtReOrderLevel.Text = "";
             }
+        }
+
+        private void txtDangerLevel_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtReOrderLevel.Text, @"^\d+$"))
+            {
+                txtReOrderLevel.Text = "";
+            }
+        }
+
+        private void lblDangerLevel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkBoxAutoReStock_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
